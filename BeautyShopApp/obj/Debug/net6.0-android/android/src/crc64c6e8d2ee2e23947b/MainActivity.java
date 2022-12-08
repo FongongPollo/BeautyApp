36026b5d29e70582420a6e513778df822d965241
FongongPollo <fongangpollo@gmail.com>
2022-12-08 20:01:54 +0100

    first version

---
+++ BeautyShopApp/obj/Debug/net6.0-android/android/src/crc64c6e8d2ee2e23947b/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64c6e8d2ee2e23947b;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("BeautyShopApp.MainActivity, BeautyShopApp", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BeautyShopApp.MainActivity, BeautyShopApp", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BeautyShopApp.MainActivity, BeautyShopApp", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
